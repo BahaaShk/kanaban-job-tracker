a86efd06914000177652d58ddd7940651ad89754
BahaaShk <bahaa.shk@gmail.com>
2026-06-11 12:58:31 +0300

    reworked sign in with new styles

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,7 +5,7 @@ import { signIn } from "@/actions/auth";
 import Link from "next/link";
 
 const LoginPage = () => {
-  const [errorMsg, setErrorMsg] = useState<string | null>("hello");
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
   const [isPending, startTransition] = useTransition();
 
   const handleSubmit = (formData: FormData) => {
@@ -19,30 +19,39 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen w-full flex justify-center items-center">
-      <div className="max-w-md rounded-md bg-bg w-full shadow-md p-4">
-        <h1 className="text-center mb-4">Welcome Back</h1>
-        <h3>Sign in to you Account</h3>
-        <form action={handleSubmit} className="mt-4 flex flex-col gap-2">
-          <label htmlFor="email">E-mail</label>
+    <div className="min-h-screen w-full flex justify-center items-center bg-bg">
+      <div className="max-w-md rounded-md bg-surface w-full shadow-xl p-8">
+        <h1 className="text-center mb-4 text-2xl font-bold">Welcome Back</h1>
+        <p className="text-text-secondary mb-6">Sign in to your account</p>
+        <form action={handleSubmit} className="mt-4 flex flex-col">
+          {/* Email */}
+          <div className=" mb-2">
+          <label htmlFor="email" className="text-sm font-medium text-text-secondary mb-2">E-mail</label>
           <input
             type="email"
-            className="border p-1"
+            className="border px-3 w-full border-border bg-surface py-1 rounded-md focus:outline-none focus:ring-2 text-text-primary"
             name="email"
             required
             placeholder="Enter your email ..."
-          />
-          <label htmlFor="password">Password</label>
-          <input type="password" className="border p-1" name="password" />
-          {errorMsg && <p className="text-danger">{errorMsg}</p>}
+            />
+            </div>
+            {/* Password */}
+            <div className=" mb-2">
+
+          <label htmlFor="password" className="text-sm font-medium text-text-secondary mb-1">Password</label>
+          <input type="password" className="border px-3 w-full border-border bg-surface py-1 rounded-md focus:outline-none focus:ring-2 text-text-primary" name="password" />
+          </div>
+          {/* error message  */}
+          {errorMsg && <p className="text-danger text-sm mt-1">{errorMsg}</p>}
           <button
             type="submit"
-            className="border p-1 w-30 justify-center items-center"
-          >
-            Submit
+            disabled={isPending}
+            className={`border bg-accent text-white py-2 px-4 rounded-md font-medium hover:bg-accent-hover mt-8 ${isPending ? "opacity-50 cursor-not-allowed" : ""}`}
+            >
+            {isPending ? "Submitting..." : "Submit"}
           </button>
         </form>
-        <p className="mt-1">
+        <p className="mt-4 text-sm text-center text-text-secondary">
           Don't have an account ?{" "}
           <Link href={"/signup"} className="text-accent">
             Sign up
